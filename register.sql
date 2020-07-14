1ad924a878ea605d7a5db93dd8e22f02f61cc878
Ighogboja Odiri <odizeeplatform@gmail.com>
2020-07-14 09:00:43 +0100

    heroku

---
+++ register.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 13, 2020 at 06:18 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `register`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reg`
+--
+
+CREATE TABLE `reg` (
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reg`
+--
+
+INSERT INTO `reg` (`email`) VALUES
+('Odizeeplatform@gmail.com'),
+('Odizeeplatform@gmail.com'),
+('Odizeeplatform@gmail.com'),
+('Odizeeplatform@gmail.com'),
+('Odizeeplatform@gmail.com'),
+('Odizeeplatform@gmail.com'),
+('Odizeeplatform@gmail.com'),
+('Odizeeplatform@gmail.com'),
+('Odizeeplatform@gmail.com'),
+('Odizeeplatform@gmail.com'),
+(''),
+('odiriighogboja'),
+(''),
+(''),
+('Odizeeplatform@gmail.com'),
+('odizeeplatform@gmail.com');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
